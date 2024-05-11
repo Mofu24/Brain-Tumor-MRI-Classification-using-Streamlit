d1d97b0dfeb7e8927ba81d03cdb635129ad683e1
Mofu24 <80823290+Mofu24@users.noreply.github.com>
2024-05-12 00:30:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import tensorflow as tf
-import PIL
 from PIL import Image, ImageOps
 import numpy as np
 
@@ -13,17 +12,8 @@ def load_model():
 # Load the model
 model = load_model()
 
-# Custom CSS for changing background color
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #f0f8ff;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Set the background color
+st.set_page_config(layout="wide", page_title="Brain Tumor MRI Classification", page_icon=":brain:")
 
 # Streamlit UI
 st.write("""
@@ -36,7 +26,7 @@ file = st.file_uploader("Choose a Brain MRI image", type=["jpg", "png"])
 # Function to preprocess the image and make predictions
 def import_and_predict(image_data, model):
     size = (150, 150)  # Match the input size with the Google Colab code
-    image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
     img = np.asarray(image)
     img = img / 255.0  # Normalize pixel values
     img_reshape = img[np.newaxis, ...]
