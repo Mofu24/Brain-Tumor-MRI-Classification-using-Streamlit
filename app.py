675d9854a823105c01f94def4b337b2a4c0ac3d9
Mofu24 <80823290+Mofu24@users.noreply.github.com>
2024-05-12 01:04:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,45 @@
-st.markdown("""
-<style>
-.container {
-    border: 2px solid black;
-    padding: 20px;
-}
-</style>
-""", unsafe_allow_html=True)
+import streamlit as st
+import tensorflow as tf
+import PIL
+from PIL import Image, ImageOps
+import numpy as np
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('fmodel.h5')
+    return model
+
+model = load_model()
+
+# Streamlit UI design
+st.sidebar.header('Group 3 - CPE 019-CPE32S6')
+st.sidebar.markdown("Ejercito, Marlon Jason")
+st.sidebar.markdown("Flores, Mico Joshua")
+st.sidebar.markdown("Flores, Marc Oliver")
+st.sidebar.markdown("Gabiano, Chris Leonard")
+st.sidebar.markdown("Gomez, Joram")
 
 st.write("""
-<div class="container">
-    <h1>Brain Tumor MRI Classification</h1>
-    <p>Select a Brain MRI image below:</p>
-</div>
-""", unsafe_allow_html=True)
+# Brain Tumor MRI Classification
+""")
 
 file = st.file_uploader("Choose a Brain MRI image", type=["jpg", "png"])
 
-# Function to import and predict
 def import_and_predict(image_data, model):
-    size = (150, 150)  
-    image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  
+    size = (150, 150)  # Match the input size with the Google Colab code
+    image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
     img = np.asarray(image)
-    img = img / 255.0  
+    img = img / 255.0  # Normalize pixel values
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
     return prediction
 
-# Display prediction
 if file is None:
-    st.text("Please upload an image file.")
+    st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
     class_names = ['Glioma', 'Meningioma', 'No Tumor', 'Pituitary']
-    result = "Prediction: " + class_names[np.argmax(prediction)]
-    st.success(result)
+    string = "OUTPUT : " + class_names[np.argmax(prediction)]
+    st.success(string)
