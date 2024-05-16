39eddc8170a4b906bbab43a98816c5efd048a7f4
Mofu24 <80823290+Mofu24@users.noreply.github.com>
2024-05-16 19:11:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,23 +64,17 @@ elif page == "Guide":
 
     glioma_image = load_image("https://drive.google.com/uc?export=view&id=1_dHlhzdvtZxzPKiby1w9N__R9uPrAXUP")
     st.image(glioma_image, caption="Glioma", use_column_width=True)
-    caption_style = "font-size: 16px; color: white; text-align: center;"
-    st.markdown("Glioma")
 
     meningioma_image = load_image("https://drive.google.com/uc?export=view&id=1gCTR9Oe4zuE3SDojoqYPMPwOupfSA9Lf")
     st.image(meningioma_image, caption="Meningioma", use_column_width=True)
-    caption_style = "font-size: 16px; color: white; text-align: center;"
-    st.markdown("Meningioma")
 
     no_tumor_image = load_image("https://drive.google.com/uc?export=view&id=1JqI8bUEW6P3PyYfGsudr_0oMxekgYLDy")
-    st.image(no_tumor_image, use_column_width=True)
-    caption_style = "font-size: 16px; color: white; text-align: center;"
-    st.markdown("No Tumor")
+    st.image(no_tumor_image, caption="No Tumor", use_column_width=True)
+
 
     pituitary_image = load_image("https://drive.google.com/uc?export=view&id=1gLzYhPu_P-ZZybapSBEE_mzTymFCd7FP")
     st.image(pituitary_image, caption="Pituitary", use_column_width=True)
-    caption_style = "font-size: 16px; color: white; text-align: center;"
-    st.markdown("<p style='{caption_style}'>Pituitary</p>")
+
     
 # About Page
 elif page == "About":
